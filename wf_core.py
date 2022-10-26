fbd6d0417beb2ff553cf4a90d58b3c3cb23b3137
MANOHAR AKULA <90580887+manoharakula@users.noreply.github.com>
2022-10-25 23:00:41 -0700

    Delete wf_core.py

--- wf_core.py
+++
@@ -1,7 +0,0 @@
-# Author : Manohar Akula
-# Date: 10/23/22
-# Project - Data Munging Assignment
-def convert(box):
-    w = box[1] - box[0]
-    h = box[3] - box[2]
-    return (box[0],box[3]-h,w,h)
